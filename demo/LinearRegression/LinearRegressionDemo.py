f76982b6c8164bd442925a35008ef049d027747c
Suprabhat Rijal <goodguysman@gmail.com>
2020-06-27 15:25:03 +0545

    Updated the demo

--- demo/LinearRegression/LinearRegressionDemo.py
+++ demo/LinearRegression/LinearRegressionDemo.py
@@ -1,12 +1,12 @@
 from SkClone import LinearRegressor
 import pandas as pd
 
+
 df = pd.read_csv('demo_dataset.csv')
 
 test_model = LinearRegressor()
 test_model.fit(df["x"], df["y"])
-prediction  = test_model.predict(df["x"][20:])
-
+prediction  = test_model.predict(df["x"])
 
 print(prediction)
 
