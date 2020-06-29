a160e170fb750ef60a46fdad9524385efe180c73
suprabhatrijal <goodguysman@gmail.com>
2020-06-29 16:05:29 +0545

    Timer

--- demo/LinearRegression/LinearRegressionDemo.py
+++ demo/LinearRegression/LinearRegressionDemo.py
@@ -1,15 +1,18 @@
 from SkClone import LinearRegressor
 import pandas as pd
+import time
 
 df = pd.read_csv('demo_dataset.csv')
 
 test_model = LinearRegressor()
+start = time.time()
 test_model.fit(df["x"], df["y"])
+stop =time.time()
 prediction  = test_model.predict(df["x"][20:])
 
 
 print(prediction)
-
+print(stop-start)
 
 
 
