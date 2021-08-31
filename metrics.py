c93dea902b0b969aa8ac0d3f42e5ae0827dbaa5b
Suprabhat Rijal <goodguysman@gmail.com>
2021-08-31 11:36:23 +0545

    Added a function to calculate mean square error in the metrics module

--- metrics.py
+++ metrics.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def mean_squared_error( y, prediction):
     if len(y.shape) < 2:
-        y = y.reshape(y.shape[0],1)
+        y = y.reshape(y.shape[0], 1)
     if len(prediction.shape) < 2:
-        prediction = prediction.reshape(prediction.shape[0],1)
+        prediction = prediction.reshape(prediction.shape[0], 1)
     return (1/y.shape[0])*np.sum((y-prediction)**2)
