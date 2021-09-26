ffac99985326c5904adc0c76a531358a08b2dd73
Suprabhat Rijal <goodguysman@gmail.com>
2021-09-26 10:04:19 +0545

    Implemented "K-Nearest Neighbor" Classifier and Regressor

--- neighbors/__init__.py
+++ neighbors/__init__.py
@@ -1,5 +1,6 @@
-from ._base import KNNClassifier
+from ._base import KNNClassifier, KNNRegressor
 
 __all__ = [
-    "KNNClassifier"
+    "KNNClassifier",
+    "KNNRegressor"
 ]
--- neighbors/_base.py
+++ neighbors/_base.py
@@ -2,9 +2,9 @@ import numpy as np
 from utility import validate, compute_distances
 
 
-class KNNClassifier:
+class KNNBase:
 
-    def __init__(self, k = 3):
+    def __init__(self, k=3):
         self.k = k
         self.X = None
         self.y = None
@@ -19,9 +19,34 @@ class KNNClassifier:
         array_of_distances = compute_distances(self.X, x)
         for i in range(len(array_of_distances)):
             distances = array_of_distances[i]
-            values = np.take( self.y,np.argpartition(distances, self.k)[:self.k])
-            counts = np.bincount(values)
-            predictions[i] =np.argmax(counts)
+            values = np.take(self.y, np.argpartition(distances, self.k)[:self.k])
+            predictions[i] = self._getpredictions(values)
 
         return predictions
 
+    def _getpredictions(self, values):
+        raise NotImplementedError()
+
+
+class KNNClassifier(KNNBase):
+
+    def __init__(self, k=3):
+        super().__init__(k)
+
+    def __repr__(self):
+        return "KNNClassifier()"
+
+    def _getpredictions(self, values):
+        return np.argmax(np.bincount(values))
+
+
+class KNNRegressor(KNNBase):
+
+    def __init__(self, k=3):
+        super().__init__(k)
+
+    def __repr__(self):
+        return "KNNClassifier()"
+
+    def _getpredictions(self, values):
+        return np.mean(values)
